f1e9ecbb7755d44db4186fcc009fdca57de74135
Sergey Arsentev <einse@yandex.ru>
2019-08-31 08:32:22 +0300

    Rewrite

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # trigger4
 
-Now use python3. (I have used the v3.6.3.)
-Again, you must create a folder "trigger-index" before the first launch.
+Use python3. (I have tested it on v3.7.3.)
+
+You must create a folder "trigger-index" at first.
+
+<p xmlns:dct="http://purl.org/dc/terms/" xmlns:vcard="http://www.w3.org/2001/vcard-rdf/3.0#">
+  <a rel="license"
+     href="http://creativecommons.org/publicdomain/zero/1.0/">
+    <img src="http://i.creativecommons.org/p/zero/1.0/88x31.png" style="border-style: none;" alt="CC0" />
+  </a>
+  <br />
+  To the extent possible under law,
+  <a rel="dct:publisher"
+     href="http://github.com/einse">
+    <span property="dct:title">Sergey Arsentev</span></a>
+  has waived all copyright and related or neighboring rights to
+  <span property="dct:title">Trigger</span>.
+This work is published from:
+<span property="vcard:Country" datatype="dct:ISO3166"
+      content="RU" about="http://github.com/einse">
+  Russian Federation</span>.
+</p>
+
--- trigger.py
+++ trigger.py
@@ -1,69 +1,76 @@
-import os
-import time
+# To the extent possible under law,
+# Sergey Arsentev (http://github.com/einse)
+# has waived all copyright
+# and related or neighboring rights to Trigger.
+# This work is published from: Russian Federation.
+# More on this: see README and/or
+# http://creativecommons.org/publicdomain/zero/1.0/
 
-start_path = '/'
-tags = ['-мкрк', '-диз', 'мкрк-', 'диз-']
-count_of_target_files_in_folder = 0
-folders_collection = {}
-current_folder_number = 0
+import sys, os, time
 
-current_time__unix = time.localtime()
-current_time__human_cn\
-    = time.strftime('%Y-%m-%d_%H-%M-%S', current_time__unix)
-index_root = 'trigger-index'
-folders_index_path\
-    = './' + index_root + '/'\
-    + current_time__human_cn + '_folders-index.txt'
-files_index_path\
-    = './' + index_root + '/'\
-    + current_time__human_cn + '_files-index.txt'
+script_path = os.path.dirname(os.path.abspath(__file__))
+path_to_start = script_path
+tags = []
+shifted_argv = sys.argv[1:]
+for i, arg in enumerate(shifted_argv):
+    if os.path.sep in arg:
+        path_to_start = arg
+    else:
+        tags.append(arg)
+if len(tags) == 0:
+    print('No tags provided.\n')
+    sys.exit();
 
-for current_folder, folders, files in os.walk(start_path):    
-    count_of_target_files_in_folder = 0
-    current_folder_number = current_folder_number + 1
-    for current_file in files:
-        for current_tag in tags:
-            if current_tag in current_file:
-                count_of_target_files_in_folder\
-                    = count_of_target_files_in_folder + 1
-                if count_of_target_files_in_folder == 1:
-                    current_path_of_file\
-                        = current_folder + '/' + current_file
-                    date_of_the_first_target_file_in_folder__unix\
-                        = time.gmtime(os.stat(current_path_of_file).st_mtime)
-                    date_of_the_first_target_file_in_folder__human_cn\
-                        = time.strftime('%Y-%m-%d'\
-                        , date_of_the_first_target_file_in_folder__unix)
-                    target_folder_id\
-                        = date_of_the_first_target_file_in_folder__human_cn\
-                        + '-f'\
-                        + str(current_folder_number)
-                with open(files_index_path, 'a') as f:
-                    target_file_id = target_folder_id + '-ff'\
-                        + str(count_of_target_files_in_folder)
-                    files_index_string = '{}\t{}\t{}'\
-                        .format\
-                        ( target_folder_id\
-                        , target_file_id\
-                        , current_file\
-                        )
-                    f.write(files_index_string)
-                    f.write('\n')
-                break
-        continue # searching files
-    if count_of_target_files_in_folder > 0:
-        folders_collection[target_folder_id]\
-            = [len(files)\
-            , count_of_target_files_in_folder\
-            , current_folder]
-with open(folders_index_path, 'a') as f:
-    for i, id_ in enumerate(folders_collection):
-        folders_index_string = '{}\t{}\t{}\t{}'\
-            .format\
-            ( folders_collection[id_][0]\
-            , folders_collection[id_][1]\
-            , id_\
-            , folders_collection[id_][2]\
-            )
-        f.write(folders_index_string)
-        f.write('\n')
+count_of_hits = 0
+dirs_with_hits = {}
+dir_no = 0
+
+now = time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime())
+path_for_indices = os.path.join(script_path, 'trigger-index')
+if not os.path.exists(path_for_indices):
+    print('''Output folder ("trigger-index") doesn't exist.\n''')
+    sys.exit();
+path_template_for_index = os.path.join(path_for_indices, now + '_')
+index_of_folders = path_template_for_index + 'folders-index.txt'
+index_of_files   = path_template_for_index + 'files-index.txt'
+
+def generate_dir_id(dirname, filename, number):
+    file_path = os.path.join(dirname, filename)
+    timestamp = time.gmtime(os.stat(file_path).st_mtime)
+    return time.strftime('%Y-%m-%d', timestamp) + '-f' + str(number)
+
+def make_row(*any_args):
+    str_args = []
+    for any_arg in any_args:
+        str_args.append(str(any_arg))
+    return '\t'.join(str_args)
+
+with open(index_of_files, 'a') as f:
+    for a_dir, _, files in os.walk(path_to_start):
+        count_of_hits = 0
+        dir_no += 1
+        for a_file in files:
+            for a_tag in tags:
+                if a_tag in a_file:
+                    count_of_hits += 1
+                    if count_of_hits == 1:
+                        dir_id = generate_dir_id(a_dir, a_file, dir_no)
+                    hit_id = dir_id + '-ff' + str(count_of_hits)
+                    row = make_row(dir_id, hit_id, a_file)
+                    f.write(row + '\n')
+                    break #matching_tags
+                else:
+                    continue #matching_tags
+            continue #searching_files
+        if count_of_hits > 0:
+            dirs_with_hits[dir_id] = [len(files), count_of_hits, a_dir]
+
+with open(index_of_folders, 'a') as f:
+    for i, dir_id in enumerate(dirs_with_hits):
+        row = make_row(
+            dirs_with_hits[dir_id][0],
+            dirs_with_hits[dir_id][1],
+            dir_id,
+            dirs_with_hits[dir_id][2],
+        )
+        f.write(row + '\n')
